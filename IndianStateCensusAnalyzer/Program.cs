4e94814d24103567afaf46ba52e54b360d1893f3
ShubhamsinghRahulsingh <shubh234singh@gmail.com>
2022-12-15 08:22:27 +0530

    (Shubham)Add: Project Folder Structure Created

---
+++ IndianStateCensusAnalyzer/Program.cs
@@ -0,0 +1,11 @@
+﻿using System;
+namespace IndianStateCensusAnalyzer
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to IndianStateCensusAnalyzer Problem");
+        }
+    }
+}
